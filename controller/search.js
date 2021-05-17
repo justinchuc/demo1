46a128918218c4a8aa33fa17178240922b5cc2b2
ehuitz <64435894+ehuitz@users.noreply.github.com>
2021-05-17 10:05:56 -0600

    goood so far

--- controller/search.js
+++ controller/search.js
@@ -42,56 +42,48 @@ $(function () {
     }).done(function (data) {
       
       let results = JSON.parse(data);
-     // console.log('The data is', results);
+     console.log('The data is', results);
+     let cardContainer;
+
+     let createTaskCard = (results) => {
+     
+         let card = document.createElement('div');
+         card.className = 'card shadow cursor-pointer';
+     
+         let cardBody = document.createElement('div');
+         cardBody.className = 'card-body';
+     
+         let text = document.createElement('h5');
+         text.innerText = results.text;
+         text.className = 'card-title';
+     
+         let ename= document.createElement('div');
+         ename.innerText = results.ename;
+         ename.className = 'card-color';
+     
+     
+         cardBody.appendChild(text);
+         cardBody.appendChild(ename);
+         card.appendChild(cardBody);
+         cardContainer.appendChild(card);
+     
+     }
+     
+     let initListOfTasks = () => {
+         if (cardContainer) {
+             document.getElementById('card-container').replaceWith(cardContainer);
+             return;
+         }
+     
+         cardContainer = document.getElementById('card-container');
+         results.forEach((task) => {
+             createTaskCard(task);
+         });
+     };
+     initListOfTasks();
       if (results.success == true) {
-    //     var jobList = [{
-    //         "text": "Help",
-    //         "type": "No way",
-    //     },
-    //     {
-    //         "text": "city",
-    //         "type": "green",
-    //     }
-    // ];
-
-        let cardContainer;
-
-        let createTaskCard = (results) => {
-        
-            let card = document.createElement('div');
-            card.className = 'card shadow cursor-pointer';
-        
-            let cardBody = document.createElement('div');
-            cardBody.className = 'card-body';
-        
-            let text = document.createElement('h5');
-            text.innerText = results.text;
-            text.className = 'card-title';
-        
-            let type= document.createElement('div');
-            type.innerText = results.type;
-            type.className = 'card-color';
-        
-        
-            cardBody.appendChild(text);
-            cardBody.appendChild(type);
-            card.appendChild(cardBody);
-            cardContainer.appendChild(card);
-        
-        }
-        
-        let initListOfTasks = () => {
-            if (cardContainer) {
-                document.getElementById('card-container').replaceWith(cardContainer);
-                return;
-            }
-        
-            cardContainer = document.getElementById('card-container');
-            jobList.forEach((task) => {
-                createTaskCard(task);
-            });
-        };
-        initListOfTasks();
+   
+    
         
       } else {
         $("#message_failure").html("<strong>Failure</strong>  " + results.message);
@@ -104,6 +96,6 @@ $(function () {
       console.log("failure.");
       alert("Sorry. Server unavailable. ");
     }).always(function () {
-      console.log("Complete.");
+      //console.log("Complete.");
     });
   }
--- model/search.php
+++ model/search.php
@@ -11,7 +11,7 @@ if ($action == "search") {
     $tID = mysqli_real_escape_string($link, $_POST["tHandler"]);
     $locationID = mysqli_real_escape_string($link, $_POST["location"]);
 
-    $data["results"] = search($link, $link, $tID, $locationID);
+    $data["results"] = search($link,$tID, $locationID);
     $data["success"] = true;
   } else {
     $data["success"] = false;
@@ -21,29 +21,35 @@ if ($action == "search") {
 function search($link, $empID, $location)
 {
 
-  $stmt = $link->prepare("SELECT * FROM tasks WHERE locationID='$location' AND taskStatus='Open';");
+  $stmt = $link->prepare("SELECT tasks.taskID AS tID, tasks.employerID AS eID, tasks.taskTypeID AS ttID, tasks.taskName as tName,
+  tasks.taskDescription AS tDesc, tasks.taskDateBegin AS tDBegin, tasks.locationID as lID,
+  CONCAT(employer.employerFName,' ', employer.employerLName) as eName, taskType.taskTypeName as ttName, location.locationName as lName
+  FROM tasks
+  LEFT JOIN taskType ON tasks.taskTypeID = taskType.taskTypeID
+  LEFT JOIN employer ON tasks.employerID = employer.employerID
+  LEFT JOIN location ON tasks.locationID = location.locationID
+  WHERE tasks.locationID='$location' AND tasks.taskStatus='Open';");
   $jobList = array();
   if ($stmt->execute()) {
     $result = $stmt->get_result();
     if ($result->num_rows > 0) {
       while ($row = $result->fetch_assoc()) {
-        $skills = $row["taskTypeID"];
+        $skills = $row["ttID"];
         $stmt1 = $link->prepare("SELECT * FROM taskHandlerSkills WHERE taskHandlerID = '$empID';");
         if ($stmt1->execute()) {
           $result1 = $stmt1->get_result();
           while ($row1 = $result1->fetch_assoc()) {
             if ($skills == $row1["taskTypeID"]) {
-              $id = $row["taskTypeID"];
-              $typeName = $row["taskTypeName"];
-              $employerID = $row["employerID"];
-              $taskTypeID = $row["taskTypeID"];
-              $taskName = $row["taskName"];
-              $taskDescription = $row["taskDescription"];
-              $dateBegin = $row["taskDateBegin"];
-              $dateEnd = $row["taskDateEnd"];
-              $sRange = $row["taskPriceSRange"];
-              $eRange = $row["taskPriceERange"];
-              $jobList[] = array('id' => $id, 'text' => $typeName, 'emid' => $employerID, 'tasktype' => $taskTypeID, 'taskName' => $taskName, 'description' => $taskDescription, 'datebegin' => $dateBegin, 'dateend' => $dateEnd, 'startRange' => $sRange, 'endRange' => $dateEnd);
+              $id = $row["tID"];
+              $typeName = $row["tName"];
+              $employerID = $row["eID"];
+              $employerName = $row["eName"];
+              $taskTypeID = $row["ttID"];
+              $taskName = $row["tName"];
+              $taskDescription = $row["tDesc"];
+              $dateBegin = $row["tDBegin"];
+              $success = true;
+              $jobList[] = array('success'=> true, 'id' => $id, 'text' => $typeName, 'emid' => $employerID, 'ename' => $employerName, 'tasktype' => $taskTypeID, 'taskName' => $taskName, 'description' => $taskDescription, 'datebegin' => $dateBegin);
             }
           }
         }
